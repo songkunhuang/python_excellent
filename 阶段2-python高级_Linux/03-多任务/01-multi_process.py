7eb0ebbb6f087e463e6ed8db488494e78f7c3fb6
songkunhuang <songkunhuang@163.com>
2022-06-12 17:01:23 +0800

    html 基础

--- 阶段2-python高级_Linux/03-多任务/01-multi_process.py
+++ 阶段2-python高级_Linux/03-多任务/01-multi_process.py
@@ -1,13 +1,11 @@
 import multiprocessing
 import time
 
-
 # 跳舞任务
 def dance():
     for i in range(5):
         print("Dancing...\n")
         time.sleep(0.2)
-
 
 # 唱歌任务
 def sing():
@@ -15,17 +13,14 @@ def sing():
         print("Sing...\n")
         time.sleep(0.2)
 
-#  创建跳舞的子进程
-#  group：
-#  target：
-#  name:
-
-
-dance_process = multiprocessing.Process(target=dance, name='P1-Dance Process')
-sing_process = multiprocessing.Process(target=sing, name='P2-Sing Process')
-
-#  启动子进程执行对应任务
 if __name__ == '__main__':
+    #  创建跳舞的子进程
+    #  group：
+    #  target：
+    #  name:
+    dance_process = multiprocessing.Process(target=dance, name='P1-Dance Process')
+    sing_process = multiprocessing.Process(target=sing, name='P2-Sing Process')
+    #  启动子进程执行对应任务
     dance_process.start()
     sing_process.start()
 
--- 阶段2-python高级_Linux/03-多任务/02-get_process_id.py
+++ 阶段2-python高级_Linux/03-多任务/02-get_process_id.py
@@ -2,7 +2,6 @@ import multiprocessing
 import time
 import os
 
-
 # 跳舞任务
 def dance():
     # 获取进程编号
@@ -13,8 +12,28 @@ def dance():
     for i in range(10):
         print("dancing...")
 # 唱歌任务
+def sing():
+    # 获取当前进程编号
+    print("sing:", os.getpid())
+
+    # 获取当前进程
+    print("sing current process:", multiprocessing.current_process())
+
+
+    for i in range(5):
+        print("singing...")
+        time.sleep(0.2)
 
 # 获取当前进程编号
+if __name__ == '__main__':
+    # 获取主进程编号
+    print("main:", os.getpid())
+#     获取主进程
+    print("main current process:", multiprocessing.current_process())
 
+    dance_process = multiprocessing.Process(target=dance, name="dance process", group=None)
+    sing_process = multiprocessing.Process(target=sing, name="sing process", group=None)
 
-# 获取
+    # 启动子进程
+    dance_process.start()
+    sing_process.start()
